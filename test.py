5814fbeff5842e8adff2e489dc0e0f971905258f
shreesammatjoshi <shreesammat.joshi@accenture.com>
2026-06-16 13:47:24 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,3 +7,13 @@ def login(user_input):
 def get_user(id):
     query = "SELECT * FROM users WHERE id = " + id
     return query
+
+password = "supersecret123"
+api_key = "abc123xyz"
+
+def login(user_input):
+    eval(user_input)
+    
+def get_user(id):
+    query = "SELECT * FROM users WHERE id = " + id
+    return query
