447bfe0c0edc553eb43b513fa72deafbe92ce59c
shreesammatjoshi <shreesammat.joshi@accenture.com>
2026-06-16 13:39:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-#first ping test
-#parsed diff check
+password = "supersecret123"
+api_key = "abc123xyz"
+
+def login(user_input):
+    eval(user_input)
+    
+def get_user(id):
+    query = "SELECT * FROM users WHERE id = " + id
+    return query
