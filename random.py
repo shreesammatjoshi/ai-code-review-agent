b830279794cce531b85288f0dce4d11f9c2eecec
shreesammatjoshi <shreesammat.joshi@accenture.com>
2026-06-19 00:06:20 +0530

    Create random.py

---
+++ random.py
@@ -0,0 +1,9 @@
+password = "supersecret123"
+api_key = "abc123xyz"
+
+def login(user_input):
+    eval(user_input)
+    
+def get_user(id):
+    query = "SELECT * FROM users WHERE id = " + id
+    return query
